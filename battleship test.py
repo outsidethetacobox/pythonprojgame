7e6a486887ce65a35e6cfddbd2dfdde29e4f1249
kkandikonda <47435978+kkandikonda@users.noreply.github.com>
2019-04-20 16:04:49 -0400

    Add files via upload

--- battleship test.py
+++ battleship test.py
@@ -1,41 +1,109 @@
 from random import randint
 
-board = []
+# user will attack this board
+attackCpuBoard = []
 
+# cpu will attack this board
+unseenboard =[]
+
+# board that user will attack
 for x in range(5):
-    board.append(["0"] * 5)
+    attackCpuBoard.append(["0"] * 5)
 
-def print_board(board):
-    for row in board:
-        print (" ".join(row))
-print("lets play battleship")
-print_board(board)
+# board that cpu will attack
+for i in range(5):
+    unseenboard.append(["0"] * 5)
 
-def random_row(board):
-    return randint(0, len(board) - 1)
+user_set_row = int(input("set row: "))
+user_set_col = int(input("set col: "))
 
-def random_col(board):
-    return randint(0, len(board[0]) - 1)
-ship_row = random_row(board)
-ship_col = random_col(board)
-print(ship_row)
-print(ship_col)
+# print board that will show on screen that user will attack
+def print_board(attackCpuBoard):
+    for row in attackCpuBoard:
+        print(" ".join(row))
 
-for turn in range(4):
-    print("turn",turn + 1)
-    guess_row = int(input("Guess row:"))
-    guess_col = int(input("Guess col:"))
+# print board that cpu will attack
+def cpu_attack_board(unseenboard):
+    for hiderow in unseenboard:
+        print(" ".join(hiderow))
 
-    if guess_row == ship_row and guess_col == ship_col:
-        print("Congrats you suck my battleship")
+print("Lets play battleship!")
+print("below is what you attacked")
+print_board(attackCpuBoard)
+print()
+print()
+print("below is what cpu will attack")
+cpu_attack_board(unseenboard)
+
+
+# random define to place cpu ships
+def random_row(attackCpuBoard):
+    return randint(0, len(attackCpuBoard) - 1)
+
+# random define to place cpu ships
+def random_col(attackCpuBoard):
+    return randint(0, len(attackCpuBoard[0]) - 1)
+
+# cpu picks random row for ship
+def cpu_random_row(unseenboard):
+    return randint(0, len(unseenboard) - 1)
+# cpu picks random col for ship
+def cpu_random_col(unseenboard):
+    return randint(0, len(unseenboard[0]) - 1)
+
+# set cpu ships
+cpu_set_ship_row = random_row(attackCpuBoard)
+cpu_set_ship_col = random_col(attackCpuBoard)
+print("cpu set row at: ", cpu_set_ship_row)
+print("cpu set col at: ", cpu_set_ship_col)
+
+turn = 1
+while turn < 10:
+    print()
+    print("turn = ", turn)
+    # USER TURN
+    # user will guess a row to attack
+    user_guess_row = int(input("guess row: "))
+    user_guess_col = int(input("guess col: "))
+
+    # if hit
+    if user_guess_row == cpu_set_ship_row and user_guess_col == cpu_set_ship_col:
+        print("Congrats you have sunk the cpu battleship!")
+        break
+    # if invalid
     else:
-        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print("this number is not in the ocean")
-        elif(board[guess_row][guess_col] == "X"):
-            print("You already guessed that")
+        if(user_guess_row < 0 or user_guess_row > 4) or (user_guess_col < 0 or user_guess_col > 4):
+            print("value not in the ocean")
+        elif(attackCpuBoard[user_guess_row][user_guess_col] == "X"):
+            print("You have already guessed that")
+            turn = turn - 1
         else:
-            print("You missed my battleship")
-            board[guess_row][guess_col] = "X"
-            if turn == 3:
-                print("Game over")
-        print_board(board)
+            print("You missed the cpu battleship")
+            attackCpuBoard[user_guess_row][user_guess_col] = "X"
+    print_board(attackCpuBoard)
+
+              
+    # CPU TURN
+    cpu_guess_row = cpu_random_row(unseenboard)
+    cpu_guess_col = cpu_random_col(unseenboard)
+    print("cpu guessed row: ", cpu_guess_row)
+    print("cpu guessed col: ", cpu_guess_col)
+    if cpu_guess_row == user_set_row and cpu_guess_col == user_set_col:
+        print("The cpu has sunk your battleship!")
+        break
+    else:
+        if (unseenboard[cpu_guess_row][cpu_guess_col] == "X"):
+            print("The cpu has already guessed that")
+        else:
+            print("The cpu has missed your battleship")
+            unseenboard[cpu_guess_row][cpu_guess_col] = "X"
+        cpu_attack_board(unseenboard)
+    turn = turn + 1
+    
+    
+            
+        
+
+                          
+        
+                 
